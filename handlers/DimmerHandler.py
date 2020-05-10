6d8f8ffbfede6f8e9f61e8056cb1b2117552fca0
Ashish Pandey <ashish.pandey@gmail.com>
2020-05-11 00:34:30 +0800

    pass dimmer id from handler

--- handlers/DimmerHandler.py
+++ handlers/DimmerHandler.py
@@ -6,12 +6,12 @@ class DimmerHandler(tornado.web.RequestHandler):
     def initialize(self, util):
         self.util = util
 
-    def get(self, dimmerId):
-        self.write(str(self.util.get_dimmer_value()))
+    def get(self, dimmer_id):
+        self.write(str(self.util.get_dimmer_value(dimmer_id)))
 
-    def post(self, dimmerId):
+    def post(self, dimmer_id):
         value = int(self.request.body)
-        self.util.set_dimmer_value(value)
-        self.write("set dimmer to " + str(value))
+        self.util.set_dimmer_value(dimmer_id, value)
+        self.write("set dimmer " + dimmer_id + " to " + str(value))
 
 
